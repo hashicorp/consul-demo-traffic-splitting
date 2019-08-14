659bb22c7df07f32881b5f4fa3244ed47bfce15c
Nic Jackson <jackson.nic@gmail.com>
2019-08-14 12:07:14 +0100

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# If we do not need to register a service just run the command
-if [ -z "$SERVICE_JSON" ]; then
-  exec "$@"
-  exit $?
-fi
-
 # Wait until Consul can be contacted
-
 until consul members; do
   echo "Waiting for Consul to start"
   sleep 1
 done
+
+# If we do not need to register a service just run the command
+if [ ! -z "$SERVICE_JSON" ]; then
+  # register the service with consul
+  echo "Registering service with consul $SERVICE_JSON"
+  consul services register ${SERVICE_JSON}
+  
+  # make sure the service deregisters when exit
+  trap "consul services deregister ${SERVICE_JSON}" SIGINT SIGTERM EXIT
+fi
 
 # register any central config
 if [ ! -z "$CENTRAL_CONFIG" ]; then
@@ -23,15 +26,10 @@ if [ ! -z "$CENTRAL_CONFIG" ]; then
   done
 fi
 
-# register the service with consul
-echo "Registering service with consul $SERVICE_JSON"
-consul services register ${SERVICE_JSON}
-
-# make sure the service deregisters when exit
-trap "consul services deregister ${SERVICE_JSON}" SIGINT SIGTERM EXIT
-
+# Run the command 
 exec "$@" &
 
+# Block using tail so the trap will fire
 tail -f /dev/null &
 PID=$!
 wait $PID
